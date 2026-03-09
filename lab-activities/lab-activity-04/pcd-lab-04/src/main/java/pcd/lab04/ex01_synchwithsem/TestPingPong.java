2be17ae25f8c200bd66de6f4445f908693785ae3
Alessandro Ronchi - alessandro.ronchi7@studio.unibo.it <alessandro.ronchi7@studio.unibo.it>
2026-03-09 13:57:11 +0100

    Added comment in TestPingPong

--- lab-activities/lab-activity-04/pcd-lab-04/src/main/java/pcd/lab04/ex01_synchwithsem/TestPingPong.java
+++ lab-activities/lab-activity-04/pcd-lab-04/src/main/java/pcd/lab04/ex01_synchwithsem/TestPingPong.java
@@ -12,7 +12,7 @@ import java.util.concurrent.Semaphore;
 public class TestPingPong {
 	public static void main(String[] args) {
 		final Semaphore pingEvent = new Semaphore(0);
-		final Semaphore pongEvent = new Semaphore(1);
+		final Semaphore pongEvent = new Semaphore(1); // Pong event has already happened at the start
 
 		new Pinger(pingEvent, pongEvent).start();
 		new Ponger(pingEvent, pongEvent).start();	
