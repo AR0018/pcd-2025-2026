f603b86445971d41139961fff6c295e6a2360540
Alessandro Ronchi - alessandro.ronchi7@studio.unibo.it <alessandro.ronchi7@studio.unibo.it>
2026-03-30 13:18:45 +0200

    Modified number of sorting workers in ConcurrentSort

--- lab-activities/lab-activity-02/src/main/java/pcd/lab02/ex01/ConcurrentSort.java
+++ lab-activities/lab-activity-02/src/main/java/pcd/lab02/ex01/ConcurrentSort.java
@@ -9,7 +9,7 @@ public class ConcurrentSort {
 	
 	public static void main(String[] args) {
 	
-		int nSortingWorkers = 20; 
+		int nSortingWorkers = Runtime.getRuntime().availableProcessors() + 1;
 		log("Num elements to sort: " + VECTOR_SIZE);
 		log("Num sorting workers: " + nSortingWorkers);
 	
